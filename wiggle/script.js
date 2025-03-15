4cc72db7ad0777fc300652388836c640a5173b42
Brick 🧱 <wanted.glitch@gmail.com>
2025-03-15 15:59:44 -0400

    Update script.js

--- wiggle/script.js
+++ wiggle/script.js
@@ -52,7 +52,6 @@ const createProxyUrl = (url, baseUrl) => {
     return url;
 };
 
-// Proxy endpoint must come BEFORE static file serving
 app.get('/script.js', async (req, res) => {
     const { q } = req.query; // url to proxy
 
@@ -128,12 +127,13 @@ app.get('/script.js', async (req, res) => {
     }
 });
 
-app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, '../public')));
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, '../public', 'index.html'));
 });
+
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, '../public', 'index.html'));
 });
 
 app.listen(port, () => {
