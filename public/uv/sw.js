5411d502c2bdc28327a73491287485fed3fc98e3
Artificially Intelligent <wanted.glitch@gmail.com>
2025-04-03 09:56:09 -0400

    Update sw.js

--- public/uv/sw.js
+++ public/uv/sw.js
@@ -1,1 +1,21 @@
+var cacheName = 'LVcog';
+var filesToCache = [
+  '/uv/sw.js'
+];
 
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll(filesToCache);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
