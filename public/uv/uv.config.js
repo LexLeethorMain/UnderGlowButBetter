01ec92220e8f33bec6e0502d3c3315dae8320671
LISplaysYT <85375268+LISplaysYT@users.noreply.github.com>
2025-04-03 18:25:53 -0700

    Update uv.config.js

--- public/uv/uv.config.js
+++ public/uv/uv.config.js
@@ -3,8 +3,8 @@ self.__uv$config = { //fully editted for underglow
     bare:'https://32867gd198764dg23vd23198746v4d69182736dvussis7.9882136.xyz/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
-    handler: '/uv/uv.handler.js',
-    bundle: '/uv/uv.bundle.js',
-    config: '/uv/uv.config.js',
+    handler: '/uv.handler.js',
+    bundle: '/uv.bundle.js',
+    config: '/uv.config.js',
     sw: '/uv/uv.sw.js',
 };
