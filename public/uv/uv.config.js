073dc5d2a4897db218096bb33b55034704dcac31
LISplaysYT <85375268+LISplaysYT@users.noreply.github.com>
2025-04-04 18:14:26 -0700

    Update uv.config.js

--- public/uv/uv.config.js
+++ public/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = { //fully editted for underglow
     prefix: '/uv/go/',
-    bare:'https:/bare.benrogo.net/',
+    bare:'https://bare.benrogo.net',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
